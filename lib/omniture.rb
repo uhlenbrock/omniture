2838d58b1a90eb4861b9f435cbb26af4e546506c
Bobby Uhlenbrock <bobby@uhlenbrock.us>
2009-11-19 13:24:17 -0500

    Added env controls

--- lib/omniture.rb
+++ lib/omniture.rb
@@ -47,7 +47,7 @@ class Omniture
       <!-- SiteCatalyst code version: H.20.3.
       Copyright 1996-2009 Adobe, Inc. All Rights Reserved
       More info available at http://www.omniture.com -->
-      <script language="JavaScript" type="text/javascript" src="/javascripts/s_code.js"></script>
+      <script language="JavaScript" type="text/javascript" src="#{account_config("s_code")}"></script>
       <script language="JavaScript" type="text/javascript"><!--
       /* You may give each page an identifying name, server, and channel on the next lines. */
       
@@ -60,7 +60,7 @@ class Omniture
       <script language="JavaScript" type="text/javascript"><!--
       if(navigator.appVersion.indexOf('MSIE')>=0)document.write(unescape('%3C')+'\!-'+'-')
       //--></script><noscript><a href="http://www.omniture.com" title="Web Analytics"><img
-      src="http://#{config(:account)["trackingServer"]}/b/ss/#{config(:account)["s_account"]}/1/H.20.3--NS/0"
+      src="http://#{account_config("trackingServer")}/b/ss/#{account_config("s_account")}/1/H.20.3--NS/0"
       height="1" width="1" border="0" alt="" /></a></noscript><!--/DO NOT REMOVE/-->
       <!-- End SiteCatalyst code version: H.20.3. -->
     OUT
@@ -123,6 +123,10 @@ class Omniture
     
     def config(key)
       @@omniture_config[key]
+    end
+    
+    def account_config(key)
+      @@omniture_config[:account][Rails.env][key]
     end
   
 end
